279158299cc4687e47fea2d55d6a8574ab7d397f
Rodrigo Setti <rodrigosetti@gmail.com>
2013-02-15 22:55:21 -0800

    Several improvements
     - Handle EDAMExceptions
     - Cache decorator accepts time-out argument
     - Use a templating language for better pages and error pages
     - Implement a static file serving system
     - Redirects /user/ to default blog: /user/blog/
     - Redirects / to default /rsetti/everblogs

--- everblog.py
+++ everblog.py
@@ -5,9 +5,13 @@ A Web application that transforms an Evernote public notebook into a blog.
 """
 
 import memcache
+import os
 import re
 from urlparse import parse_qsl
 
+import jinja2
+
+from evernote.edam.error.ttypes import EDAMUserException, EDAMSystemException, EDAMNotFoundException
 from evernote.edam.notestore import NoteStore
 from evernote.edam.userstore import UserStore
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
@@ -19,28 +23,44 @@ PAGE_SIZE = 50
 #: The memcache server list
 MEMCACHE_SERVERS = ["localhost:11211"]
 
+#: The directory to find static files
+STATIC_ROOT = 'static'
+
+#: Default redirection from root /
+DEFAULT_ROOT = '/rsetti/everblog/'
+
+#: Default puburi name to redirect from username
+DEFAULT_PUBURI = 'blog'
+
 #: Used to create ids from/to guids
 ID_SYMBOLS = '0123456789abcdefghijklmnopqrstuvwxyz'
 
 #: The evernote user store thrift API URL
 EVERNOTE_USER_STORE_URL = "https://www.evernote.com/edam/user"
 
+#: Seconds in an hour
+HOUR = 60*60
+
 class cached(object):
     """Cache using memcache method's return values using their positional
        parameters as kes.
     """
 
-    def __init__(self, procedure):
-        self.procedure = procedure
+    def __init__(self, timeout=0):
+        self.timeout = timeout
 
-    def __call__(self, *args):
+    def __call__(self, procedure):
         global cache
-        key = ':'.join(str(arg) for arg in args)
-        value = cache.get(key)
-        if value is None:
-          value = self.procedure(*args)
-          if value is not None: cache.set(key, value)
-        return value
+
+        def decorated(*args):
+            key = ':'.join(str(arg) for arg in args)
+            value = cache.get(key)
+            if value is None:
+              value = procedure(*args)
+              if value is not None: cache.set(key, value, self.timeout)
+            return value
+
+        return decorated
 
 class Index(object):
     """A wrapper to a collection of posts. Index."""
@@ -85,12 +105,15 @@ def user_store_connect():
     procotol = TBinaryProtocol.TBinaryProtocol(http_client)
     return UserStore.Client(procotol)
 
-@cached
+@cached()
 def get_user(username):
     """Get user information for the username."""
-    return user_store_connect().getPublicUserInfo(username)
+    try:
+        return user_store_connect().getPublicUserInfo(username)
+    except EDAMNotFoundException as e:
+        raise NotFoundException(data="User does not exist.")
 
-@cached
+@cached(12*HOUR)
 def get_notes(note_store_url, notebook_guid, offset, limit):
     """Get a list of notes metadata from the notebook."""
     note_store = note_store_connect(note_store_url)
@@ -98,13 +121,16 @@ def get_notes(note_store_url, notebook_guid, offset, limit):
     result_spec = NoteStore.NotesMetadataResultSpec(includeTitle=True)
     return note_store.findNotesMetadata("", note_filter, offset, limit, result_spec)
 
-@cached
+@cached(48*HOUR)
 def get_notebook(note_store_url, user_id, puburi):
     """Get the public notebook for this user_id and public URL"""
     note_store = note_store_connect(note_store_url)
-    return note_store.getPublicNotebook(user_id, puburi)
+    try:
+        return note_store.getPublicNotebook(user_id, puburi)
+    except EDAMNotFoundException as e:
+        raise NotFoundException(data="Notebook does not exist or is not public.")
 
-@cached
+@cached(24*HOUR)
 def get_note(note_store_url, guid):
     """Get note with content from this guid."""
     note_store = note_store_connect(note_store_url)
@@ -114,25 +140,28 @@ def get_note(note_store_url, guid):
                               withResourcesRecognition=False,
                               withResourcesAlternateData=False)
 
-@cached
 def get_index(username, puburi, page, page_size):
     """Get the index object from this username, puburi and page."""
     user = get_user(username)
-    if not user: raise NotFoundException()
     notebook = get_notebook(user.noteStoreUrl, user.userId, puburi)
-    if not notebook: raise NotFoundException()
     notes = get_notes(user.noteStoreUrl, notebook.guid,
                       (page-1)*page_size,
                       page_size)
     return Index(notebook.name, notes)
 
-@cached
 def get_post(username, puburi, post_id):
     user = get_user(username)
     notebook = get_notebook(user.noteStoreUrl, user.userId, puburi)
-    if not notebook: raise NotFoundException()
     note = get_note(user.noteStoreUrl, id_to_guid(post_id))
     return Post(note)
+
+ROOT_RE = re.compile(r'^/?$')
+def root_handler():
+    raise HttpRedirectPermanently(DEFAULT_ROOT)
+
+USER_RE = re.compile(r'^/([\w_\-\+\.]{1,255})/?$')
+def user_handler(username):
+    raise HttpRedirectPermanently("/%s/%s" % (username, DEFAULT_PUBURI))
 
 INDEX_RE = re.compile(r'^/([\w_\-\+\.]{1,255})/([\w_\-\+\.]+)/?$')
 def index_handler(username, puburi, page=1, **kwars):
@@ -149,37 +178,59 @@ def index_handler(username, puburi, page=1, **kwars):
     index = get_index(username, puburi, page, PAGE_SIZE)
 
     # render index into HTML
-    yield "<h1>%s</h1>" % index.name
-    yield "<ul>"
-    for post in index.posts:
-        yield ('<li><a href="/%s/%s/%s" title="%s">%s</a></li>' %
-               (username, puburi, post['id'], post['title'], post['title']))
-    yield "</ul>"
-    if index.has_next:
-        yield ('<p><a href="/%s/%s/?page=%d" title="next page">Next page (%d)</a></p>' %
-               (username, puburi, page+1, page+1))
+    template = template_env.get_template('index.html')
+    data = template.render(index=index, username=username, puburi=puburi, page=page).encode('utf-8')
+    return [data]
 
 POST_RE = re.compile(r'^/([\w_\-\+\.]{1,255})/([\w_\-\+\.]+)/([a-z0-9]+)/?$')
-def post_handler(username, puburi, title_slug, **kwargs):
+def post_handler(username, puburi, post_id, **kwargs):
     """Handler for the /username/puburi/title-slug URL, shows the post's
        content.
     """
     # get post information
-    post = get_post(username, puburi, title_slug)
+    post = get_post(username, puburi, post_id)
 
     # render post to HTML
-    yield "<h1>%s</h1>" % post.title
-    yield '<div id="content">'
-    yield post.html
-    yield "</div>"
+    template = template_env.get_template('post.html')
+    data = template.render(post=post, username=username, puburi=puburi, post_id=post_id).encode('utf-8')
+    return [data]
+
+STATIC_RE = re.compile(r'^/static/(\w[\w\-_\/]+(?:\.css|\.js|\.html))$')
+@cached()
+def static_handler(path):
+    """Debug handler for static files"""
+    filename = os.path.join(STATIC_ROOT, path)
+    if not os.path.exists(filename):
+        raise NotFoundException(data="")
+    with open(filename) as f:
+        return [f.read()]
 
 #: The list of matching regular expression paired with the callable handler
-HANDLERS = ((INDEX_RE, index_handler),
-            (POST_RE, post_handler))
+HANDLERS = ((ROOT_RE, root_handler),
+            (USER_RE, user_handler),
+            (INDEX_RE, index_handler),
+            (POST_RE, post_handler),
+            (STATIC_RE, static_handler))
 
-class NotFoundException(Exception):
-    """Exception to be raise by the handlers if they want to return a 404."""
-    pass
+class HttpException(Exception):
+    """A non-OK http status"""
+    def __init__(self, status, headers=(), data=''):
+        super(HttpException, self).__init__(status)
+        self.status = status
+        self.headers = headers
+        self.data = data
+
+class NotFoundException(HttpException):
+    """A 404 http status"""
+    def __init__(self,
+                 headers=[("Content-Type", "text/plain")],
+                 data="Not found."):
+        super(NotFoundException, self).__init__("404 Not found", headers, data)
+
+class HttpRedirectPermanently(HttpException):
+    def __init__(self, location):
+        super(HttpRedirectPermanently, self).__init__("301 Moved Permanently",
+                                                      [("Location", location)])
 
 def application(environment, start_response):
     """WSGI Application handler: check if the URL matches any of the handlers.
@@ -196,16 +247,9 @@ def application(environment, start_response):
                 break
         else:
             raise NotFoundException()
-    except NotFoundException:
-        start_response("404 Not found", [
-            ("Content-Type", "text/plain"),
-        ])
-        return ["Not found."]
-    except Exception:
-        start_response("500 Internal Server Error", [
-            ("Content-Type", "text/plain"),
-        ])
-        return ["Server error."]
+    except HttpException as e:
+        start_response(e.status, e.headers)
+        return [e.data]
 
     start_response("200 OK", [
         ("Content-Type", "text/html"),
@@ -214,4 +258,9 @@ def application(environment, start_response):
 
 # Start memcache client
 cache = memcache.Client(MEMCACHE_SERVERS)
+
+#: Create the template loader (for HTML pages)
+template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+                        os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                     "templates"))))
 
