32466ffc5b31c780f00978705c3d5ff4b68435eb
Rodrigo Setti <rodrigosetti@gmail.com>
2013-02-16 02:09:31 -0800

    Forget to add config file

---
+++ config.py
@@ -0,0 +1,19 @@
+
+#: Number of post per page
+PAGE_SIZE = 50
+
+#: The memcache server list
+MEMCACHE_SERVERS = ["localhost:11211"]
+
+#: The directory to find static files (debug)
+STATIC_ROOT = 'static'
+
+#: The url root of static files
+STATIC_URL = '/static'
+
+#: Default redirection from root /
+DEFAULT_ROOT = '/rsetti/everblog/'
+
+#: Default puburi name to redirect from username
+DEFAULT_PUBURI = 'blog'
+
